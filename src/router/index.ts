4eec598b755dc3e7fb90e2b23e6a20476ba501dc
Larry Brobbey <larrybe.dev@gmail.com>
2024-08-25 15:04:45 -0700

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import ExploreView from '@/views/ExploreView.vue'
 import StoryView from '@/views/StoryView.vue'
@@ -6,7 +6,7 @@ import UserView from '@/views/UserView.vue'
 import FourOhFour from '@/components/FourOhFour.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
